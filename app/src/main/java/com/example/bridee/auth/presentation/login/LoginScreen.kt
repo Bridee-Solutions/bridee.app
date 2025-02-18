bd072af64b209bb86248e1d5bab57564dd889318
Ian Silva Santos <ian.santos@sptech.school>
2025-02-17 21:57:51 -0300

    feat: Continuando estilização da tela de login.

--- app/src/main/java/com/example/bridee/auth/presentation/login/LoginScreen.kt
+++ app/src/main/java/com/example/bridee/auth/presentation/login/LoginScreen.kt
@@ -1,29 +1,42 @@
 package com.example.bridee.auth.presentation.login
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
 
+@Preview(showBackground = true)
 @Composable
 fun LoginScreen(){
 
@@ -36,18 +49,57 @@ fun LoginScreen(){
         )
     }
 
+    val windowWidthDp = LocalConfiguration.current.screenWidthDp.dp
+
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.SpaceEvenly
     ) {
         Row {
-            Text("Não tem uma conta? ")
-            Text("Cadastre-se")
+            Text(
+                text = "Não tem uma conta? ",
+                fontSize = TextUnit(
+                    value = 20.0f,
+                    type = TextUnitType.Sp
+                )
+            )
+            Text(
+                text = "Cadastre-se",
+                style = TextStyle(
+                    color = Color(0xFFB55557),
+                    fontSize = TextUnit(
+                        value = 20.0f,
+                        type = TextUnitType.Sp
+                    )
+                )
+            )
         }
-        Text("Bridee.")
-        Text("O match perfeito para o dia dos seus sonhos")
-        Text("Bem vindo de volta!")
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "bridee.",
+                fontSize = TextUnit(
+                    value = 48.0f,
+                    type = TextUnitType.Sp
+                )
+            )
+            Text(
+                text = "O match perfeito para o dia dos seus sonhos",
+                fontSize = TextUnit(
+                    value = 16.0f,
+                    type = TextUnitType.Sp
+                )
+            )
+        }
+        Text(
+            text = "Bem vindo de volta!",
+            fontSize = TextUnit(
+                24.0f,
+                TextUnitType.Sp
+            )
+        )
         TextField(
             value = loginState.email.value,
             onValueChange = {
@@ -56,10 +108,27 @@ fun LoginScreen(){
             placeholder = {
                 Icon(
                     imageVector = Icons.Filled.Email,
-                    contentDescription = "Email"
+                    contentDescription = "Email",
+                    tint = Color(0xFFB55557)
                 )
-                Text("E-mail")
-            }
+                Text(
+                    text = "E-mail",
+                    modifier = Modifier.offset(x = 30.dp),
+                    color = Color(0xFFC2C2C2)
+                )
+            },
+            modifier = Modifier.width(windowWidthDp - 45.dp)
+                .border(
+                    BorderStroke(width = 2.dp, color = Color(0xFF999999)),
+                    shape = RoundedCornerShape(25)
+                ),
+            colors = TextFieldDefaults.colors(
+                unfocusedContainerColor = Color(0xFFF5F5F5),
+                focusedContainerColor = Color(0xFFF5F5F5),
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent
+            ),
+            singleLine = true
         )
         TextField(
             value = loginState.senha.value,
@@ -70,17 +139,61 @@ fun LoginScreen(){
             placeholder = {
                 Icon(
                     imageVector = Icons.Filled.Lock,
-                    contentDescription = "Senha"
+                    contentDescription = "Senha",
+                    tint = Color(0xFFB55557)
                 )
-                Text("Senha")
-            }
+                Text(
+                    text = "Senha",
+                    modifier = Modifier.offset(x = 30.dp),
+                    color = Color(0xFFC2C2C2)
+                )
+            },
+            modifier = Modifier.width(windowWidthDp - 45.dp)
+                .border(
+                    BorderStroke(width = 2.dp, color = Color(0xFF999999)),
+                    shape = RoundedCornerShape(30)
+                ),
+            colors = TextFieldDefaults.colors(
+                unfocusedContainerColor = Color(0xFFF5F5F5),
+                focusedContainerColor = Color(0xFFF5F5F5),
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent
+            ),
+            singleLine = true,
         )
-        Text("Esqueceu sua senha?")
+        Text(
+            text = "Esqueceu sua senha?",
+            fontSize = TextUnit(value = 14.0f, type = TextUnitType.Sp)
+        )
         Button(
-            onClick = {}
+            onClick = {},
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFFD86B67)
+            ),
+            modifier = Modifier.height(50.dp)
+                .width(windowWidthDp - 120.dp),
+            shape = RoundedCornerShape(25)
         ) {
             Text("Entrar")
         }
-        Text("Você é um assessor?")
+        Row {
+            Text(
+                text = "Você é um assessor? ",
+                fontSize = TextUnit(
+                    value = 20.0f,
+                    type = TextUnitType.Sp
+                )
+            )
+            Text(
+                text = "Clique aqui.",
+                style = TextStyle(
+                    color = Color(0xFFB55557),
+                    fontSize = TextUnit(
+                        value = 20.0f,
+                        type = TextUnitType.Sp
+                    )
+                )
+            )
+        }
     }
 }
