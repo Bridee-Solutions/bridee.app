0ae80a66092b015b3f3bae504d647dfcde5e9587
Ian Silva Santos <ian.santos@sptech.school>
2025-03-27 14:45:05 -0300

    feat: Iniciando lógica de criação de casal

--- app/src/main/java/com/example/bridee/auth/domain/RegistrationSharedViewModel.kt
+++ app/src/main/java/com/example/bridee/auth/domain/RegistrationSharedViewModel.kt
@@ -2,10 +2,13 @@ package com.example.bridee.auth.domain
 
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import com.example.bridee.auth.data.AuthEndpoints
 import com.example.bridee.auth.presentation.registration.RegistrationState
+import com.example.bridee.core.api.ApiInstance
 
 class RegistrationSharedViewModel: ViewModel() {
 
+    private val usuarioService = ApiInstance().createService(AuthEndpoints::class.java)
     private val _state = mutableStateOf(RegistrationState())
     val sharedRegistrationObject = _state
 
@@ -14,7 +17,10 @@ class RegistrationSharedViewModel: ViewModel() {
     }
 
     fun salvarCasal(){
-
+        val createdCasal = usuarioService.createCasal(this._state.value)
+        if(createdCasal.code() != 201){
+            //TODO: Toast
+        }
     }
 
 }
