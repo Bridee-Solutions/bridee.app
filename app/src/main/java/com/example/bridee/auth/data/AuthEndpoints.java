85871a00b04bd85cd0cd5e0163532b3493cc6878
Ian Silva Santos <ian.santos@sptech.school>
2025-03-27 14:44:51 -0300

    refactor: alterando retorno da criação de casal.

--- app/src/main/java/com/example/bridee/auth/data/AuthEndpoints.java
+++ app/src/main/java/com/example/bridee/auth/data/AuthEndpoints.java
@@ -2,11 +2,12 @@ package com.example.bridee.auth.data;
 
 import com.example.bridee.auth.presentation.registration.RegistrationState;
 
+import okhttp3.Response;
 import retrofit2.http.Body;
 import retrofit2.http.POST;
 
 public interface AuthEndpoints {
 
     @POST("/usuarios")
-    void createCasal(@Body RegistrationState casalRequest);
+    Response createCasal(@Body RegistrationState casalRequest);
 }
