ab97a22e04120b13ce22f08923ae9de4392834c5
ian15963 <ian.santos@sptech.school>
2025-04-08 21:59:37 -0300

    feat: Adicionando divulgar por whatsapp e adicionar convite.

--- app/src/main/java/com/example/bridee/convidados/presentation/ConvidadoScreen.kt
+++ app/src/main/java/com/example/bridee/convidados/presentation/ConvidadoScreen.kt
@@ -15,13 +15,19 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.MailOutline
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.bridee.ui.components.ferramentas_section.domain.Tool
@@ -30,7 +36,7 @@ import com.example.bridee.ui.components.ferramentas_section.presentation.screens
 @Composable
 fun ConvidadoScreen(navController: NavController){
 
-    val screenWidth = LocalConfiguration.current.screenWidthDp.dp
+    val screenWidth = LocalConfiguration.current.screenWidthDp
     val scrollState = rememberScrollState()
 
     Column(
@@ -48,14 +54,14 @@ fun ConvidadoScreen(navController: NavController){
         ) {
             Column(
                 modifier = Modifier
-                    .width(screenWidth - 60.dp)
+                    .width(screenWidth.dp - 60.dp)
                     .height(120.dp)
                     .border(
-                        width = 2.dp,
+                        width = 1.dp,
                         color = Color(0xC3C3C3C9),
                         shape = RoundedCornerShape(15)
                     )
-                    .padding(start = 10.dp, top = 10.dp, bottom = 10.dp),
+                    .padding(start = 15.dp, top = 10.dp, bottom = 10.dp),
                 verticalArrangement = Arrangement.SpaceEvenly
             ) {
                 Row (
@@ -93,9 +99,73 @@ fun ConvidadoScreen(navController: NavController){
                 Row{
                     Text("Relatório de convidados")
                 }
-                Row{
-                    Text("5 convidados irão comparecer")
+                Row(
+                    modifier = Modifier.offset(y = (-10).dp)
+                ){
+                    Text(
+                        text = "5 convidados irão comparecer",
+                        fontSize = TextUnit(
+                            value = 12.0f,
+                            type = TextUnitType.Sp
+                        )
+                    )
                 }
+            }
+        }
+
+        Row(
+            modifier = Modifier
+                .width((screenWidth * 0.75).dp)
+                .offset(x = 30.dp, y = 15.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Column(
+                modifier = Modifier
+                    .width((screenWidth * 0.35).dp)
+                    .height(100.dp)
+                    .border(
+                        width = 1.dp,
+                        color = Color(0xC3C3C3C9),
+                        shape = RoundedCornerShape(15)
+                    )
+                    .padding(start = 10.dp, top = 10.dp, bottom = 10.dp),
+                verticalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Ícone para adição de novo convite"
+                )
+                Text(
+                    text = "Adicionar convite",
+                    fontSize = TextUnit(
+                        type = TextUnitType.Sp,
+                        value = 16.0F
+                    )
+                )
+            }
+            Column(
+                modifier = Modifier
+                    .width((screenWidth * 0.35).dp)
+                    .height(100.dp)
+                    .border(
+                        width = 1.dp,
+                        color = Color(0xC3C3C3C9),
+                        shape = RoundedCornerShape(15)
+                    )
+                    .padding(start = 10.dp, top = 10.dp, bottom = 10.dp),
+                verticalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Icon(
+                    imageVector = Icons.Default.MailOutline,
+                    contentDescription = "Ícone para adição de novo convite"
+                )
+                Text(
+                    text = "Divulgar por WhatsApp",
+                    fontSize = TextUnit(
+                        type = TextUnitType.Sp,
+                        value = 16.0F
+                    )
+                )
             }
         }
 
