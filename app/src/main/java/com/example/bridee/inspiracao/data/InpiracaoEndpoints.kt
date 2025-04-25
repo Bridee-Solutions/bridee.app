089a2e20200f7b9c4ccc290af2cb97415f19bc3d
ian15963 <ian.santos@sptech.school>
2025-04-25 11:47:11 -0300

    feat: Adicionando anotação @GET do retrofit.

--- app/src/main/java/com/example/bridee/inspiracao/data/InpiracaoEndpoints.kt
+++ app/src/main/java/com/example/bridee/inspiracao/data/InpiracaoEndpoints.kt
@@ -1,9 +1,11 @@
 package com.example.bridee.inspiracao.data
 
 import com.example.bridee.inspiracao.domain.PexelsResponseDto
+import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface InpiracaoEndpoints {
 
+    @GET("pexels/images")
     suspend fun findImages(@Query("query") query: String): PexelsResponseDto
 }
