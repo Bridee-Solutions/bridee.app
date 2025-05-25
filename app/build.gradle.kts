119953da5765e6048296c1f0fbf22f219a666b52
ian15963 <ian.santos@sptech.school>
2025-05-24 21:46:48 -0300

    feat: Adicionando injeção de dependência.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation(libs.androidx.benchmark.macro)
     implementation(libs.androidx.datastore.preferences.v100)
     implementation(libs.coil.compose)
+    implementation(libs.koin.androidx.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
