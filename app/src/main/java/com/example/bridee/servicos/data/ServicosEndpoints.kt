ca039aa6d55b69e8602cee505dacf8c67a512c6b
ian15963 <ian.santos@sptech.school>
2025-05-25 23:33:11 -0300

    feat: Adicionando fetch de categorias e fornecedores da subcategoria.

---
+++ app/src/main/java/com/example/bridee/servicos/data/ServicosEndpoints.kt
@@ -0,0 +1,21 @@
+package com.example.bridee.servicos.data
+
+import com.example.bridee.core.pagination.PaginationResponse
+import com.example.bridee.servicos.domain.AssociadoResponseDto
+import com.example.bridee.servicos.domain.Categoria
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface ServicosEndpoints {
+
+    @GET("categorias-servicos")
+    suspend fun getCategories(): Response<PaginationResponse<Categoria>>
+
+    @GET("fornecedores/details/subcategoria/{subcategoriaId}")
+    suspend fun getFornecedoresBySubcategoria(
+        @Path("subcategoriaId") id: Int,
+        @Query("nome") nome: String
+    ): Response<PaginationResponse<AssociadoResponseDto>>
+}
