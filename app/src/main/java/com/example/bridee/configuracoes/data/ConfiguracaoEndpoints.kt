cc473a4bb0eb5063fa9aa4dbe16689ec84106644
ian15963 <ian.santos@sptech.school>
2025-06-04 09:48:59 -0300

    feat: Criando os endpoints da tela de configurações.

--- app/src/main/java/com/example/bridee/configuracoes/data/ConfiguracaoEndpoints.kt
+++ app/src/main/java/com/example/bridee/configuracoes/data/ConfiguracaoEndpoints.kt
@@ -1,9 +1,15 @@
 package com.example.bridee.configuracoes.data
 
+import com.example.bridee.calculadora.domain.CasalResponse
+import com.example.bridee.configuracoes.domain.ConfiguracaoCasal
+import com.example.bridee.configuracoes.domain.ConfiguracaoCasamento
+import com.example.bridee.home.domain.HomeCasamentoResponse
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.Multipart
+import retrofit2.http.PATCH
 import retrofit2.http.POST
 import retrofit2.http.Part
 
@@ -15,4 +21,14 @@ interface ConfiguracaoEndpoints {
         @Part file: MultipartBody.Part,
         @Part("metadata") metadata: RequestBody
     ): Response<Unit>
+
+    @PATCH("casamentos")
+    suspend fun updateCasamentoInfo(
+            @Body configuracaoCasamento: ConfiguracaoCasamento
+    ): Response<HomeCasamentoResponse>
+
+    @PATCH("casais")
+    suspend fun updateCasalInfo(
+        @Body configuracaoCasal: ConfiguracaoCasal
+    ): Response<CasalResponse>
 }
